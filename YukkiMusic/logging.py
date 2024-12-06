3286a1ee334bfce0685274f045fe055163f84786
Teamonix <unioxo@outlook.com>
2024-12-06 14:08:26 +0530

    Create logging.py

---
+++ YukkiMusic/logging.py
@@ -0,0 +1,21 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+# store all logs
+logging.basicConfig(
+    format="[%(name)s]:: %(message)s",
+    level=logging.INFO,
+    datefmt="%H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=(1024 * 1024 * 5), backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+
+logging.getLogger("apscheduler").setLevel(logging.ERROR)
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+
+LOGGER = logging.getLogger("YUKKI")
